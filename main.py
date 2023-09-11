151b8a061d87d187f8569d18b308f01e851aff32
kwa_laa <khoaddnguyen@gmail.com>
2023-09-11 14:45:00 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import requests
+
+# Extracting titles and links of the articles
+
+
+response = requests.get("https://news.ycombinator.com/")
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "html.parser")
+print(soup.title)
+
+# Find FIRST
+# article_tag = soup.find(name="a", rel="noreferrer")
+# article_text = article_tag.getText()
+# article_link = article_tag.get("href")
+# article_upvote = soup.find(name="span", class_="score").getText()
+#
+# print(article_text)
+# print(article_link)
+# print(article_upvote)
+
+# Find ALL
+articles = soup.find_all(name="a", rel="noreferrer")
+article_texts = []
+article_links = []
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get("href")
+    article_links.append(link)
+
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+
+largest_number = max(article_upvotes)
+largest_index = article_upvotes.index(largest_number)
+
+print(article_texts[largest_index])
+print(article_links[largest_index])
+
+# print(article_texts)
+# print(article_links)
+# print(article_upvotes)
